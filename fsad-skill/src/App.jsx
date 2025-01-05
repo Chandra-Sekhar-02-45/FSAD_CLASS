e734413d5772809e0105b0100d9b37852838d81e
Chandra Sekhar <rvcsraju@gmail.com>
2025-01-05 10:21:42 +0530

    Every Thing Changed To New One And Its Functional

--- fsad-skill/src/App.jsx
+++ fsad-skill/src/App.jsx
@@ -4,36 +4,66 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div className="container">
-        <div className="header">
-            <img className="logo" src="ICONS/logo.png" alt='' />
-            <h1>JOB PORTAL</h1>
-              <div className='user'>
-                    <h2>
-                      Sign In
-                    </h2>
-                  <img className="user_logo" src="ICONS/user.png" alt='' />
+    <>  <div className='container-fluid'>
+          <div className='row'>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary col-12">
+          <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>       
+            <a className="navbar-brand" href="#">
+              <h1 className="com_name d-flex flex-row justify-content-centre">
+                <img src="ICONS/logo.png" alt="logo" className="logo" />
+                <span className='head1 m-2'>JOB PORTAL</span>
+              </h1>
+            </a>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-1">
+                  <li className="nav-item d-flex flex-row justify-content-centre">
+                  <li className="nav-item dropdown">
+          <a className="nav-link" href="#" role="button"       data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link active" aria-current="page" href="#">
+          <h5 className="nav_text m-2">Available Jobs</h5> 
+          </a>    
+          </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Technical</a></li>
+                    <li className="dropdown-divider"></li>
+                    <li><a className="dropdown-item" href="#">Non Technical</a></li>
+                  </ul>
+        </li>
+                  
+                      <div className="scrolling-message">
+                          <h5 className='nav_text m-2'>
+                            <span>Worlds Best Job Portal For Skilled People And Non Skilled People</span></h5>               
+                      </div>
+                  </li>
+                      
+                </ul>
               </div>
+              <div>
+             </div>  
         </div>
-        <div className="content">
-          <div className='text1'>Worlds best platform for searching your jobs</div>
-          <div className='text2'>Get your job now</div>
-          <div className='text3'>NOW AVAILABLE</div>
+        <div className="d-flex flex-row justify-content-center">
+          <div>
+            <button className="button_prof" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <i className="fa-solid fa-magnifying-glass icon"></i>
+            </button>
+          </div>
+          <div>
+            <button className="button_prof m-auto">
+            <i className="fa-solid fa-bell icon"></i>
+            </button>
+          </div>
+          <div>
+            <button className="button_prof m-auto">
+            <i className="fa-solid fa-user icon"></i>
+            </button>
+          </div>
         </div>
-        <div className="footer">
-            <h2>Follow Us</h2>
-            <div className='social'>
-                  <img className="social" src="ICONS/linkedin.png" alt="" />
-                  <img className="social" src="ICONS/facebook.png" alt="" />
-                  <img className="social" src="ICONS/twitter.png" alt="" />
-            </div>
-            <div className='placeholder'>
-              <input type="text" className='searchText' placeholder='Search by skill' />
-            </div>
-      
+      </nav>    
+          </div>
         </div>
-      </div>
     </>
   )
 }
