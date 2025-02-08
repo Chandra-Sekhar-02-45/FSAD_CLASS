045e5e510b76b049c9f6262ac1af10d16d4dd11b
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-02-08 10:56:29 +0530

    exp5 done

--- fsad-practical/Lab-2/responsive_design/src/main.jsx
+++ fsad-practical/Lab-2/responsive_design/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Template from './components/Exp_1/Template.jsx'
-import Parent from './components/Exp_3/Parent.jsx'
-import 'exp_5.jsx'
+// import Template from './components/Exp_1/Template.jsx'
+// import Parent from './components/Exp_3/Parent.jsx'
+import Exp_5 from './components/Exp_5/Exp_5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      {/* <Template/> */}
-     <Parent/>
+     {/* <Parent/> */}
+      < Exp_5/>
   </StrictMode>
 )
+
